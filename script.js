37426f1d81c2366d253899bb5e5ff1189d64a3fe
Maria Eduarda <maria.miranda@gmail.com>
2025-10-27 09:07:55 -0300

    START: Criando HTML, CSS, JS e adicionando imagens

---
+++ script.js
@@ -0,0 +1,33 @@
+// Programa - Modelo 1
+window.document.addEventListener("DOMContentLoaded", function(){
+    // Botão: Letra Maiúscula
+    window.document.querySelector("#btnMaiuscula").addEventListener("click", function(){
+        let inputText = document.querySelector("#input-text").value;
+        document.querySelector("#result").innerHTML = inputText.toUpperCase();
+    });
+
+    // Botão: Letra Minúscula
+    window.document.querySelector("#btnMinuscula").addEventListener("click", function(){
+        let inputText = document.querySelector("#input-text").value;
+        document.querySelector("#result").innerHTML = inputText.toLowerCase();
+    });
+
+    //Botão: Primeira Letra Maiúscula
+    window.document.querySelector("#btnCapitalize").addEventListener("click", function(){
+        let inputText = document.querySelector("#input-text").value;
+        document.querySelector("#result").innerHTML = inputText.charAt(0).toUpperCase() + inputText.slice(1).toLowerCase();
+    });
+
+    // Botão: Limpar campos
+    window.document.querySelector("#btnLimpar").addEventListener("click", function(){
+        document.querySelector("#input-text").value = " ";
+        document.querySelector("#result").innerHTML = " ";
+        document.querySelector("#input-text").focus();
+    });
+
+    // Botão-Extra: Inverter o texto
+    window.document.querySelector("#btnInverter").addEventListener("click", function(){
+        let inputText = document.querySelector("#input-text").value;
+        document.querySelector("#result").innerHTML = inputText.split("").reverse().join("")
+    });
+});
